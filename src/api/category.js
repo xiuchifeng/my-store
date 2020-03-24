bf34c795f7f052ac23218906416d7fbbd2ed4b7e
xiuchif <xiuchif@aliyun.com>
2020-03-24 20:00:57 +0800

    删除了src文件夹

--- src/api/category.js
+++
@@ -1,6 +0,0 @@
-import request from "./request"
-
-
-export const reqCategory = () => request('/category')
-export const reqSubcategory = maitKey => request('/subcategory', { maitKey })
-export const reqCategoryDetail = (miniWallkey, type) => request('/subcategory/detail', { miniWallkey, type })
--- src/api/home.js
+++
@@ -1,7 +0,0 @@
-import request from "./request.js"
-
-//轮播图数据
-export const reqHomeImg=()=>request("/home/multidata")
-
-//请求home组件的推荐数据
-export const reqHomeGoods = (type,page)=>request('/home/data',{type,page})
